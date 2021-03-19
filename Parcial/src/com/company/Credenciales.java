ee7dd2075f784b2fce8d76649d93431058728fd9
Luis-cast <78997814+Luis-cast@users.noreply.github.com>
2021-03-18 22:53:19 -0500

    parcialsubida

---
+++ Parcial/src/com/company/Credenciales.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Credenciales {
+
+
+  String name[]  = new String[200];
+      String pw[]  = new String[200];
+      double cash[]  = new double[200];
+
+}
